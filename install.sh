518a279aa284137fe654788e2c4584a47731a2c9
ilmars <9987548+darksworm@users.noreply.github.com>
2021-10-05 22:14:29 +0300

    improve output

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ PARTITIONS_HOME_PARTITION_SIZE_MB=2048
 PARTITIONS_ROOT_PARTITION_SIZE_MB=16384
 PARTITIONS_EFI_PARTITION_SIZE_MB=512
 
-echo -n "Checking network connectivity "
+echo -n "Checking network connectivity -> "
 ping -c 1 -W 3 google.com &> /dev/null || ( echo "ERROR" && exit 1 ) && echo "OK"
 
 timedatectl set-ntp true
