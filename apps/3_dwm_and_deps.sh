ab6d7e93a4baea1c9c9025322459a359369f87d3
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 12:51:27 +0300

    Update 3_dwm_and_deps.sh

--- apps/3_dwm_and_deps.sh
+++ apps/3_dwm_and_deps.sh
@@ -4,7 +4,7 @@ chmod +x $BIN_DIR/defaultmon
 ln -sf $BIN_DIR/defaultmon /bin/defaultmon
 
 sudo -i -u ilmars sh << EOF
-yay -S --noconfirm xbanish nitrogen sxhkd xcape cbatticon networkmanager pa-applet-git blueman bluetooth-autoconnect xfce4-power-manager compton xautolock redshift st brave-bin
+yay -S --noconfirm xbanish nitrogen sxhkd xcape cbatticon networkmanager pa-applet-git xfce4-power-manager compton xautolock redshift st brave-bin
 EOF
 
 cd /opt
