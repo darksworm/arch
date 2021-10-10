2979978cd528d70460add2c311377cd9f03095d0
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 19:05:48 +0300

    Create 96_work.sh

---
+++ apps/96_work.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+sudo -i -u work sh << EOF
+yay -S --noconfirm slack
+scp ilmars@192.168.1.111:/storage/tank/laptop-backup/work ~/.config/backedup
+rm -rf ~/.ssh ~/.aws ~/.config/JetBrains
+ln -sf /home/work/.config/backedup/ssh ~/.ssh
+ln -sf /home/work/.config/backedup/aws ~/.aws
+mkdir -p /home/work/.config/JetBrains/DataGrip2021.2
+ln -sf /home/work/.config/backedup/datagrip ~/.config/JetBrains/DataGrip2021.2
+EOF
