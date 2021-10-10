e62d72691207567f52dd43bb9678538dce6f15bf
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 19:34:32 +0300

    Update 96_work.sh

--- apps/96_work.sh
+++ apps/96_work.sh
@@ -2,13 +2,17 @@
 
 sudo -i -u work sh << EOF
 yay -S --noconfirm slack rsync
+
 scp ilmars@192.168.1.111:/storage/tank/laptop-backup/work ~/.config/backedup
-rm -rf ~/.ssh ~/.aws ~/.config/JetBrains
-ln -sf /home/work/.config/backedup/ssh ~/.ssh
-ln -sf /home/work/.config/backedup/aws ~/.aws
+
+rm -rf /home/work/.ssh /home/work/.aws /home/work/.config/JetBrains /home/work/dev
+
+ln -sf /home/work/.config/backedup/ssh /home/work/.ssh
+ln -sf /home/work/.config/backedup/aws /home/work/.aws
+
 mkdir -p /home/work/.config/JetBrains/DataGrip2021.2
-ln -sf /home/work/.config/backedup/datagrip ~/.config/JetBrains/DataGrip2021.2
+ln -sf /home/work/.config/backedup/datagrip /home/work/.config/JetBrains/DataGrip2021.2
 
 mkdir ~/dev
-rsync -avz deploy-box:/home/ec2-user/* ~/dev/
+rsync -avz deploy-box:/home/ec2-user/\* ~/dev/
 EOF
