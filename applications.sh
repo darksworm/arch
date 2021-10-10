427a9083855ec73a51897c0c5525a6857e3b433b
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 12:25:46 +0300

    install dwm

--- applications.sh
+++ applications.sh
@@ -34,3 +34,7 @@ chmod +x $BIN_DIR/defaultmon
 ln -sf $BIN_DIR/defaultmon /bin/defaultmon
 
 yay -S --noconfirm xbanish nitrogen sxhkd xcape cbatticon networkmanager pa-applet-git blueman bluetooth-autoconnect xfce4-power-manager compton xautolock redshift st
+
+cd /opt
+git clone https://github.com/darksworm/dwm
+cd dwm && make install
