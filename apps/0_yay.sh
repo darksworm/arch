bc0926f5b76f0459881a18584e11fd79396f9f74
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 12:44:54 +0300

    Create 0_yay.sh

---
+++ apps/0_yay.sh
@@ -0,0 +1,9 @@
+pacman -S --noconfirm go
+cd /opt
+git clone https://aur.archlinux.org/yay-git.git
+chown -R ilmars yay-git
+su ilmars -c "cd /opt/yay-git && makepkg --noconfirm"
+cd yay-git
+pacman -U --noconfirm $(ls -1 | grep tar.zst)
+cd .. && rm -rf yay-git
+pacman -R --noconfirm go
