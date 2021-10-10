98d750e81a11eff4ed2ff5cca174e9076d0d63df
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 11:43:53 +0300

    x11 base

--- applications.sh
+++ applications.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+CONF_DIR=/opt/arch/conf
+
+# video drivers
+pacman -S --noconfirm xf86-video-intel mesa
+# xorg server / x11
+pacman -S --noconfirm xorg-server xorg-xbacklight
+
+mkdir -p /etc/X11/xorg.conf.d
+cp $CONF_DIR/30-touchpad.conf /etc/X11/xorg.conf.d/
+
