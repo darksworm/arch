6e140bc709daed739e66f063145bc57691999050
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 17:58:05 +0300

    Update 5_zsh.sh

--- apps/5_zsh.sh
+++ apps/5_zsh.sh
@@ -4,11 +4,7 @@ set -e
 
 pacman -S --noconfirm zsh
 
-groupadd chsh
-sed -i '2s;^;auth sufficient  pam_wheel.so  trust group=chsh\n;' /etc/pam.d/chsh
-
 for USERNAME in ilmars work; do
-    usermod -a -G chsh $USERNAME
     sudo -i -u $USERNAME zsh << EOF
         git clone --recursive https://github.com/sorin-ionescu/prezto.git "\${ZDOTDIR:-\$HOME}/.zprezto"
         
@@ -17,10 +13,6 @@ for USERNAME in ilmars work; do
         for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
           ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
         done
-        
-        chsh -s /bin/zsh
 EOF
+    chsh -s $(which zsh) $USERNAME
 done
-
-sed -i '2d' /etc/pam.d/chsh
-groupdel chsh
