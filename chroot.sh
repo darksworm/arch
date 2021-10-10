23fbd04fa78ed6b7f8485c07f8c714d30e9d1c19
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 12:37:53 +0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -12,12 +12,9 @@ passwd
 ln -sf /usr/share/zoneinfo/Europe/Riga /etc/localtime
 hwclock --systohc
 
-sed -i 's/#lv_LV.UTF-8 UTF-8/lv_LV.UTF-8 UTF-8/g' /etc/locale.gen
-sed -i 's/en_US.UTF-8 UTF-8/#en_US.UTF-8 UTF-8/g' /etc/locale.gen
-
 locale-gen
 
-echo LANG=lv_LV.UTF-8 > /etc/locale.conf
+echo LANG=en_US.UTF-8 > /etc/locale.conf
 echo $HOSTNAME > /etc/hostname
 
 echo "127.0.0.1     localhost" >    /etc/hosts
