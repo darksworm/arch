ccc1bc5fda618ecb358698cfafe69b0095e305db
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 12:56:47 +0300

    Create 99_ide.sh

---
+++ apps/99_ide.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo -i -u ilmars sh << EOF
+yay -S --noconfirm neovim
+EOF
+
+for USERNAME in ilmars work; do
+    mkdir -p /home/$USERNAME/.config/nvim
+    ln -sf $CONF_DIR/init.vim /home/$USERNAME/.config/nvim/init.vim
+done
+
+sudo -i -u ilmars sh << EOF
+yay -S --noconfirm intellij-idea-ultimate-edition datagrip
+EOF
