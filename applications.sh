ec07fd1d76c681b21620d9c70b905dd7fc82ca6a
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 12:47:52 +0300

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -2,36 +2,12 @@
 
 set -e
 
-CONF_DIR=/opt/arch/conf
-BIN_DIR=/opt/arch/bin
+export CONF_DIR=/opt/arch/conf
+export BIN_DIR=/opt/arch/bin
 
-# yay
-pacman -S --noconfirm go
-cd /opt
-git clone https://aur.archlinux.org/yay-git.git
-chown -R ilmars yay-git
-su ilmars -c "cd /opt/yay-git && makepkg --noconfirm"
-cd yay-git
-pacman -U --noconfirm $(ls -1 | grep tar.zst)
-cd .. && rm -rf yay-git
-pacman -R --noconfirm go
-
-yay # update
-
-# video drivers
-pacman -S --noconfirm xf86-video-intel mesa xorg-xinit
-# xorg server / x11
-pacman -S --noconfirm xorg-server xorg-xbacklight
-
-mkdir -p /etc/X11/xorg.conf.d
-cp $CONF_DIR/30-touchpad.conf /etc/X11/xorg.conf.d/
-
-for USERNAME in ilmars work; do
-    mkdir -p /home/$USERNAME/.config/sxhkd
-    ln -sf $CONF_DIR/xinitrc /home/$USERNAME/.xinitrc
-    ln -sf $CONF_DIR/xresources /home/$USERNAME/.Xresources
-    ln -sf $CONF_DIR/xmodmap /home/$USERNAME/.Xmodmap
-    ls -sf $CONF_DIR/sxhkd /home/$USERNAME/.config/sxhkd/sxhkdrc
+for SCRIPT in $(ls -1 apps); do
+    chmod +x apps/$SCRIPT
+    ./apps/$SCRIPT
 done
 
 chmod +x $BIN_DIR/defaultmon
