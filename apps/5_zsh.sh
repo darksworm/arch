9815e9e2b3652f7ed1620d86e9fe0b4e46c2dc70
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 18:06:01 +0300

    Update 5_zsh.sh

--- apps/5_zsh.sh
+++ apps/5_zsh.sh
@@ -11,7 +11,7 @@ for USERNAME in ilmars work; do
         setopt EXTENDED_GLOB
         
         for rcfile in "\${ZDOTDIR:-\$HOME}"/.zprezto/runcoms/^README.md(.N); do
-          ln -s "$rcfile" "\${ZDOTDIR:-\$HOME}/.\${rcfile:t}"
+          ln -s "\$rcfile" "\${ZDOTDIR:-\$HOME}/.\${rcfile:t}"
         done
 EOF
     chsh -s $(which zsh) $USERNAME
