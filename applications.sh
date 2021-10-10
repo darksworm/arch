3f8be214f063715ce494687d9e261757736bf29a
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 13:13:26 +0300

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -5,9 +5,9 @@ set -e
 export CONF_DIR=/opt/arch/conf
 export BIN_DIR=/opt/arch/bin
 
-for SCRIPT in $(ls -1 apps); do
+for SCRIPT in $(ls -1 /opt/arch/apps); do
     echo
     echo "Running $SCRIPT"
-    chmod +x apps/$SCRIPT
-    ./apps/$SCRIPT
+    chmod +x /opt/arch/$SCRIPT
+    ./opt/arch/$SCRIPT
 done
