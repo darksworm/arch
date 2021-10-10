2a9554318d9087c3c3f1a5b5022a54a3afb9c5c1
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-10 12:48:37 +0300

    Update applications.sh

--- applications.sh
+++ applications.sh
@@ -9,14 +9,3 @@ for SCRIPT in $(ls -1 apps); do
     chmod +x apps/$SCRIPT
     ./apps/$SCRIPT
 done
-
-chmod +x $BIN_DIR/defaultmon
-ln -sf $BIN_DIR/defaultmon /bin/defaultmon
-
-sudo -i -u ilmars sh << EOF
-yay -S --noconfirm xbanish nitrogen sxhkd xcape cbatticon networkmanager pa-applet-git blueman bluetooth-autoconnect xfce4-power-manager compton xautolock redshift st brave-bin
-EOF
-
-cd /opt
-git clone https://github.com/darksworm/dwm
-cd dwm && make install
