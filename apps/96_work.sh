9b3e5d2a349bf45231c3fd3b9de60127ecd4d235
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-12 21:33:46 +0300

    Update 96_work.sh

--- apps/96_work.sh
+++ apps/96_work.sh
@@ -2,9 +2,7 @@
 
 sudo -i -u work sh << EOF
 yay -S --noconfirm slack-desktop rsync docker docker-compose libreoffice networkmanager-openvpn tfswitch stretchly
-
 sh $BIN_DIR/work_restore
-
 tfswitch --latest
 EOF
 
