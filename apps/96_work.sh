cb53b1e015bcbe9385a3214a5c5ee8036b70a24c
darksworm <9987548+darksworm@users.noreply.github.com>
2021-10-12 21:33:27 +0300

    Update 96_work.sh

--- apps/96_work.sh
+++ apps/96_work.sh
@@ -5,17 +5,6 @@ yay -S --noconfirm slack-desktop rsync docker docker-compose libreoffice network
 
 sh $BIN_DIR/work_restore
 
-rm -rf /home/work/.ssh /home/work/.aws /home/work/.config/JetBrains /home/work/dev
-
-ln -sf /home/work/.config/backedup/ssh /home/work/.ssh
-ln -sf /home/work/.config/backedup/aws /home/work/.aws
-
-mkdir -p /home/work/.config/JetBrains/DataGrip2021.2
-ln -sf /home/work/.config/backedup/datagrip /home/work/.config/JetBrains/DataGrip2021.2
-
-mkdir ~/dev
-rsync -avz --exclude 'mysql8' deploy-box:/home/ec2-user/\* ~/dev/
-
 tfswitch --latest
 EOF
 
