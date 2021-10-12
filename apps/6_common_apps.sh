e3b95efe75afd4d3f9558d3aa7f2f36cd6d500e8
Ilmārs Jānis Blūzmanis <ilmars.bluzmanis@edurio.com>
2021-10-12 22:12:49 +0300

    fix init and install discord

--- apps/6_common_apps.sh
+++ apps/6_common_apps.sh
@@ -9,3 +9,9 @@ echo password	optional	pam_gnome_keyring.so >> /etc/pam.d/passwd
 sudo -i -u ilmars sh << EOF
 yay -S --noconfirm brave-bin noto-fonts-emoji spotify
 EOF
+
+cd /opt
+wget 'https://discord.com/api/download?platform=linux&format=tar.gz' -O dank.tar.gz
+tar -xvf dank.tar.gz
+rm -rf dank.tar.gz
+ln -sf /bin/discord /opt/Discord/Discord
